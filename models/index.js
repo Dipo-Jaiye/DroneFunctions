7661f28ab37b01ab6400238126b17c4300a179a5
apple <oladipupo@errandpay.com>
2023-05-25 20:34:23 +0100

    feat:add audit logs to db context

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Drone = require("./drone");
 const Medication = require("./medication");
 const Payload = require("./payload");
+const Audit = require("./audit");
 
 module.exports = {
     Drone,
     Medication,
     Payload,
+    Audit,
 }
