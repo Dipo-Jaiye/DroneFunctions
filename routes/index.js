073841f7259f95c50ea43614776b9a5f9488644c
apple <oladipupo@errandpay.com>
2023-05-25 18:46:29 +0100

    feat:modify endpoint to use drone serial number and add endpoint for getting drone items

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ const router = Router();
 router.post("/drone/register", dispatchCtrl.createDrone);
 router.post("/drone/addMedication", dispatchCtrl.addMedication);
 router.get("/drones/available", dispatchCtrl.getAvailableDrones);
-router.get("/drone/:id/batteryLevel", dispatchCtrl.getDroneBatteryLevel);
+router.get("/drone/:sn/batteryLevel", dispatchCtrl.getDroneBatteryLevel);
+router.get("/drone/:sn/items", dispatchCtrl.getDroneItems);
 
 
 module.exports = router;
