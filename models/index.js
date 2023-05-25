99371e86705ceaf882bc8411c510bc4bbb5885f7
apple <oladipupo@errandpay.com>
2023-05-25 18:45:31 +0100

    feat:add payload to the context

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const Drone = require("./drone");
 const Medication = require("./medication");
+const Payload = require("./payload");
 
 module.exports = {
     Drone,
     Medication,
+    Payload,
 }
